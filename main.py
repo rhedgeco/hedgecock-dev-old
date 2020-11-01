4ddaf7771085c9ef8301df4db32400820d358bd0
rhedgeco <rhedgeco@gmail.com>
2020-11-01 01:21:27 -0700

    initial upload and setup of sanic webserver

---
+++ main.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from sanic import Sanic, response
+
+from backend.frontend_updater import FrontendUpdater
+
+app = Sanic('hedgecock_dev')
+
+main_dir = Path('.')
+frontend_dir = Path('./frontend')
+app.static('/', str(frontend_dir))
+
+
+@app.route('/')
+async def index(request):
+    return await response.file(str(frontend_dir / 'index.html'))
+
+
+frontend_updater = FrontendUpdater(main_dir, frontend_dir)
+app.add_route(frontend_updater.update_frontend, '/api/update_frontend', methods=['POST'])
+
+if __name__ == "__main__":
+    print('Serving at http://localhost/')
+    app.run(host="0.0.0.0", port=80)
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='hedgecock_dev',
+    version='1.0.0',
+    install_requires=[
+        "google-api-python-client",
+        "google-auth-httplib2",
+        "google-auth-oauthlib",
+        "cachecontrol",
+        'google',
+        'requests',
+        'sanic',
+        'websockets',
+        'dataclasses'
+    ]
+)
