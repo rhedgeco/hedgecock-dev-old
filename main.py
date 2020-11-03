dcfe0c7e91ea8fdeaed7daba0bed67da2e166026
rhedgeco <rhedgeco@gmail.com>
2020-11-02 22:00:39 -0800

    removed frontend updater stuff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from sanic import Sanic, response
 
-from backend.frontend_updater import FrontendUpdater
 from backend.route_adding import add_routes
 
 app = Sanic('hedgecock_dev')
@@ -16,10 +15,6 @@ async def index(request):
     return await response.file(str(frontend_dir / 'index.html'))
 
 add_routes(app)
-
-frontend_updater = FrontendUpdater(main_dir, frontend_dir)
-app.add_route(frontend_updater.update_frontend, '/api/update_frontend',
-              methods=['POST'])
 
 if __name__ == "__main__":
     print('Serving at http://localhost/')
