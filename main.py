41addbd19785679882dd600dbb5cb50fa7e6ce4c
rhedgeco <rhedgeco@gmail.com>
2020-11-03 16:51:05 -0800

    added templating support for frontend pages

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ from pathlib import Path
 from sanic import Sanic, response
 
 from backend.route_adding import add_routes
+from backend.templating.template_router import \
+    define_frontend_routes_with_templates
 
 app = Sanic('hedgecock_dev')
 
 main_dir = Path('.')
 frontend_dir = Path('./frontend')
-app.static('/', str(frontend_dir))
-
-
-@app.route('/')
-async def index(request):
-    return await response.file(str(frontend_dir / 'index.html'))
+template_dir = Path('./templates')
+define_frontend_routes_with_templates(app, frontend_dir, template_dir)
 
 add_routes(app)
 
