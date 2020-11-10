b583d4427073193e4966de3b68916bb15df6a7ad
rhedgeco <rhedgeco@gmail.com>
2020-11-10 14:20:39 -0800

    added static routing back to main and route adder

--- backend/route_adding.py
+++ backend/route_adding.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
 from sanic import Sanic
 
 from backend.ice_cream_or_pickle.ice_cream_or_pickle import IceCreamOrPickle
 
 
-def add_routes(app: Sanic):
+def add_routes(app: Sanic, frontend_dir: Path):
+    app.static('/', str(frontend_dir))
     app.add_route(IceCreamOrPickle.as_view(), '/ice_cream_or_pickle')
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from pathlib import Path
-from sanic import Sanic
+from sanic import Sanic, response
 
 from backend.route_adding import add_routes
-from backend.templating.template_router import \
-    define_frontend_routes_with_templates
 
 app = Sanic('hedgecock_dev')
 
 main_dir = Path('.')
 frontend_dir = Path('./frontend')
 template_dir = Path('./templates')
-define_frontend_routes_with_templates(app, frontend_dir, template_dir)
+add_routes(app, frontend_dir)
 
-add_routes(app)
+
+@app.route('/')
+async def index(request):
+    return await response.file(str(frontend_dir / 'index.html'))
+
 
 if __name__ == "__main__":
     print('Serving at http://localhost/')
