ec8cc56005cfd24eba57aeeef5fbd98cc9b55853
rhedgeco <rhedgeco@gmail.com>
2020-11-11 14:19:45 -0800

    added favicon route

--- routing/route_adding.py
+++ routing/route_adding.py
@@ -12,8 +12,12 @@ def add_routes(app: Sanic):
     async def index(request):
         return await response.file(str(home_index / 'index.html'))
 
+    async def favicon(request):
+        return await response.file(str(home_index / 'favicon.ico'))
+
     app.static('/static', str(home_index / 'static'))
     app.add_route(index, '/')
+    app.add_route(favicon, '/favicon.ico')
 
     app.add_route(ice_cream_or_pickle.IceCreamOrPickle.as_view(),
                   '/ice_cream_or_pickle')
